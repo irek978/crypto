15fb544344de6e80aff5b69ebefdc0899e1b14b1
irek978 <irek978@yahoo.it>
2015-03-07 12:55:34 +0100

    modified crypto keys and decrypto system

--- decrypto.R
+++ decrypto.R
@@ -1,4 +1,5 @@
 decrypto <-function(cryptopath) {
+    
     setwd(cryptopath)
     cryptokeys<-read.csv("cryptokey.csv")
     crypto<-read.csv("crypto.csv")
@@ -17,108 +18,91 @@ decrypto <-function(cryptopath) {
     
     CLength<-unique(max(str_length(crypto$x)))
     NRow<-length(crypto$x)
-    dfkeysx<-as.character(cryptokeys[1,2])
     
-    startkeyorig<-gsub("À","1",dfkeysx)
-    Keycforig2<-gsub("Á","2",startkeyorig) 
-    rm(startkeyorig)
-    Keycforig3<-gsub("Â","3",Keycforig2)   
-    rm(Keycforig2)
-    Keycforig4<-gsub("Ã","4",Keycforig3)   
-    rm(Keycforig3)
-    Keycforig5<-gsub("Ä","5",Keycforig4)   
-    rm(Keycforig4)
-    Keycforig6<-gsub("Å","6",Keycforig5)   
-    rm(Keycforig5)
-    Keycforig7<-gsub("Ā","7",Keycforig6)   
-    rm(Keycforig6)
-    Keycforig8<-gsub("Æ","8",Keycforig7)   
-    rm(Keycforig7)
-    Keycforig9<-gsub("Ç","9",Keycforig8)   
-    rm(Keycforig8)
-    Keycforig0<-gsub("È","0",Keycforig9)
-    rm(Keycforig9)
-    KeycforigA<-gsub("É","A",Keycforig0)
-    rm(Keycforig0)
-    KeycforigB<-gsub("Ê","B",KeycforigA)
-    rm(KeycforigA)
-    KeycforigC<-gsub("Ë","C",KeycforigB)
-    rm(KeycforigB)
-    KeycforigD<-gsub("Ē","D",KeycforigC)
-    rm(KeycforigC)
-    KeycforigE<-gsub("Ì","E",KeycforigD)
-    rm(KeycforigD)
-    KeycforigF<-gsub("Í","F",KeycforigE)
-    rm(KeycforigE)
-    KeycforigG<-gsub("Î","G",KeycforigF)
-    rm(KeycforigF)
-    KeycforigH<-gsub("Ï","H",KeycforigG)
-    rm(KeycforigG)
-    KeycforigI<-gsub("Ī","I",KeycforigH)
-    rm(KeycforigH)
-    KeycforigJ<-gsub("Ð","J",KeycforigI)
-    rm(KeycforigI)
-    KeycforigK<-gsub("Ñ","K",KeycforigJ)
-    rm(KeycforigJ)
-    KeycforigL<-gsub("Ò","L",KeycforigK)
-    rm(KeycforigK)
-    KeycforigM<-gsub("Ó","M",KeycforigL)
-    rm(KeycforigL)
-    KeycforigN<-gsub("Ô","N",KeycforigM)
-    rm(KeycforigM)
-    KeycforigO<-gsub("Õ","O",KeycforigN)
-    rm(KeycforigN)
-    KeycforigP<-gsub("Ö","P",KeycforigO)
-    rm(KeycforigO)
-    KeycforigQ<-gsub("Ø","Q",KeycforigP)
-    rm(KeycforigP)
-    KeycforigR<-gsub("Ō","R",KeycforigQ)
-    rm(KeycforigQ)
-    KeycforigS<-gsub("Œ","S",KeycforigR)
-    rm(KeycforigR)
-    KeycforigT<-gsub("Š","T",KeycforigS)
-    rm(KeycforigS)
-    KeycforigU<-gsub("þ","U",KeycforigT)
-    rm(KeycforigT)
-    KeycforigV<-gsub("Ù","V",KeycforigU)
-    rm(KeycforigU)
-    KeycforigW<-gsub("Ú","W",KeycforigV)
-    rm(KeycforigV)
-    KeycforigY<-gsub("Û","Y",KeycforigW)
-    rm(KeycforigW)
-    KeycforigX<-gsub("Ü","X",KeycforigY)
-    rm(KeycforigY)
-    dfkeys<-gsub("Ū","Z",KeycforigX)
-    rm(KeycforigX)
     
-    Ldfkeys<-str_length(dfkeys)
-    submatch<-(Ldfkeys-CLength-CLength)/3
+    dedfkeys<-as.character(cryptokeys[1,2])
+    
+    
+    Ldedfkeys<-str_length(dedfkeys)
+    submatch<-(Ldedfkeys-CLength-CLength)/3
     
     OKeyxn<-vector()
     for (l in 1:CLength) {
-        OKeyx<-substr(substr(dfkeys,1,CLength),l,l)
+        OKeyx<-substr(substr(dedfkeys,1,CLength),l,l)
         OKeyxn<-c(OKeyxn,OKeyx)
     }
     
     SKeyxn<-vector()
     for (l in 1:CLength) {
-        SKeyx<-substr(substr(dfkeys,as.numeric(CLength+1),as.numeric(CLength+CLength)),l,l)
+        SKeyx<-substr(substr(dedfkeys,as.numeric(CLength+1),as.numeric(CLength+CLength)),l,l)
         SKeyxn<-c(SKeyxn,SKeyx)
     }
     
     SKeyxn2<-vector()
     for (d in 1:submatch) {
-        SKeyx2<-substr(substr(dfkeys,as.numeric(CLength+CLength+1),as.numeric(submatch)),d,d)
+        SKeyx2<-substr(substr(dedfkeys,as.numeric(CLength+CLength+1),as.numeric(submatch+CLength+CLength)),d,d)
         SKeyxn2<-c(SKeyxn2,SKeyx2)
+    }
+    
+    LCrypto<-list()
+    for (r in 1:NRow) {
+        LCrypton<-list()
+        for (n in 1:CLength) {
+            LCryptoxn<-substr(crypto$x[r],n,n)
+            LCrypton<-c(LCrypton,LCryptoxn)
+        }
+        LCrypto<-c(LCrypto,LCrypton)
     }
     
     CFKeyorigxn<-list()
     for (d in 1:submatch) {
-        SKorig<-substr(substr(dfkeys,as.numeric(CLength+CLength+submatch+1),as.numeric(CLength+CLength+submatch+submatch)),d,d)
+        SKorig<-substr(substr(dedfkeys,as.numeric(CLength+CLength+submatch+1),as.numeric(CLength+CLength+submatch+submatch)),d,d)
         CFKeyorigxn<-c(CFKeyorigxn,SKorig)
     }
     
-    decolsplit<-split(CFKeyorigxn, 1:CLength)
+    CFKeycfrsub<-list()
+    for (d in 1:submatch) {
+        SKsub<-substr(substr(dedfkeys,as.numeric(CLength+CLength+submatch+submatch+1),as.numeric(Ldedfkeys)),d,d)
+        CFKeycfrsub<-c(CFKeycfrsub,SKsub)
+    }
+    
+    deccrypto<-cbind(LCrypto,pos=1:NRow) 
+    decfrsub<-cbind(CFKeycfrsub,pos=1:NRow)
+    decforig<-cbind(CFKeyorigxn,pos=1:NRow)
+    
+    dematchrow<-cbind(rbind(deccrypto), rbind(decfrsub))
+    decheckmatch<-as.character(dematchrow[,1])!=as.character(dematchrow[,3])
+    dematchrowl<-cbind(rbind(dematchrow),cbind(decheckmatch),cbind(SKeyxn2), cbind(decforig))
+    dedfmatch<-data.frame(dematchrowl)
+    dedimmatch<-nrow(dedfmatch) 
+    
+    deputdef<-data.frame()
+    for(r in 1:dedimmatch) {
+        deputdata<-data.frame()    
+        if (dedfmatch[r,5]==FALSE & dedfmatch[r,3]!="§" & dedfmatch[r,7]=="ø") {
+            deput<-as.character(dedfmatch[r,3])
+            deputdata<-rbind(c(deputdata,deput))
+        }
+        if (dedfmatch[r,5]==FALSE & dedfmatch[r,3]!="§" & dedfmatch[r,7]!="ø") {
+            deput<-as.character(dedfmatch[r,7])
+            deputdata<-rbind(c(deputdata,deput))
+        }
+        if (dedfmatch[r,3]=="§" & dedfmatch[r,7]=="ø") {
+            deput<-as.character(dedfmatch[r,3])
+            deputdata<-rbind(c(deputdata,deput))
+        }
+        if (dedfmatch[r,5]==TRUE & dedfmatch[r,7]!="ø" & dedfmatch[r,3]!="§") {
+            deput<-as.character(dedfmatch[r,7])
+            deputdata<-rbind(c(deputdata,deput))
+        }
+        if (dedfmatch[r,5]==TRUE & dedfmatch[r,7]=="ø" & dedfmatch[r,3]!="§") {
+            deput<-as.character(dedfmatch[r,3])
+            deputdata<-rbind(c(deputdata,deput))
+        }
+        deputdef<-cbind(c(deputdef,deputdata))
+    }
+    
+    decolsplit<-split(deputdef, 1:CLength)
     
     NColde<-length(decolsplit[[CLength]])
     
@@ -133,7 +117,7 @@ decrypto <-function(cryptopath) {
         i=i+1
     }
     
-    decrypto<-gsub("%","", decryptox)
+    decrypto<-gsub("§","", decryptox)
     decrypto
     
 }
